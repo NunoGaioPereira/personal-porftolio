53896e0e97fc6c0d49c96b63b002aef644a0b08f
Nuno Pereira <nunocgpereira@gmail.com>
2021-04-17 11:38:06 +0100

    Add contact page

--- server.js
+++ server.js
@@ -13,5 +13,9 @@ app.get('/', (req, res) => {
 	res.render('index');
 })
 
+app.get('/contact', (req, res) => {
+	res.render('contact');
+})
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
