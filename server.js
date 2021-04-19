87db6355ff7aa4a3d94ba1c3fb869afdc53805ab
Nuno Pereira <nunocgpereira@gmail.com>
2021-04-19 22:25:46 +0100

    Listen on port 80

--- server.js
+++ server.js
@@ -29,5 +29,5 @@ app.get('/contact', (req, res) => {
 	res.render('contact');
 })
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
