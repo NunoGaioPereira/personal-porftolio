4219e1e5dd222b86d91dd42b3ec7611d87577ae6
Nuno Pereira <nunocgpereira@gmail.com>
2021-04-23 00:20:56 +0100

    Adding title

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+    	transitionProperty: {
+            'width': 'width'
+        },
+    },
+  },
+  variants: {
+    extend: {
+    	translate: ['group-hover']
+    },
+  },
+  plugins: [],
+}
